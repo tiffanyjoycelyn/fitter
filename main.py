8b4e3f9257cf7c21a5e0020fd88aa099dacd4682
tiffany <tiffanylumengkewas@gmail.com>
2024-06-10 17:01:11 +0700

    fix typo

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import io
 
 model = tf.keras.models.load_model('exported_model')
-arrKey = np.array(["ayam", "daging_rendang", "dendeng_batokok", "gulai_ikan", "gulai_tambusu", "telur_bbalado", "telur_dadar", "tahu", "daun_singkong", "perkedel", "nasi", "tempe", "telur_mata_sapi", "mie", "udang"])
+arrKey = np.array(["ayam", "daging_rendang", "dendeng_batokok", "gulai_ikan", "gulai_tambusu", "telur_balado", "telur_dadar", "tahu", "daun_singkong", "perkedel", "nasi", "tempe", "telur_mata_sapi", "mie", "udang"])
 le2 = LabelEncoder()
 arrKey = le2.fit_transform(arrKey)
 
@@ -35,5 +35,6 @@ def main ():
         prediction = make_prediction(x)
         st.image(Image.open(image))
         st.write(prediction)
+        
 if __name__ == '__main__':
     main()
